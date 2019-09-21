3c71f0667cf2e7878ff3588196955ae3c5ac7c96
中华小当家 <xiaodangjia@gov.cn>
2019-09-21 10:12:17 +0800

    optimize

--- collect-common/collect-util/src/test/java/collect/util/AlgorithmUtilTest.java
+++ collect-common/collect-util/src/test/java/collect/util/AlgorithmUtilTest.java
@@ -2,77 +2,96 @@ package collect.util;
 
 import com.common.collect.util.AlgorithmUtil;
 import com.common.collect.util.StringUtil;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-import static com.common.collect.util.AlgorithmUtil.signature;
-
 /**
  * Created by nijianfeng on 2019/7/21.
  */
+
+@Slf4j
 public class AlgorithmUtilTest {
+
+    public static void hash(String content) {
+        log.info("########## 散列算法 ###########");
+        for (AlgorithmUtil.HashAlgorithm algorithm : AlgorithmUtil.HashAlgorithm.values()) {
+            log.info(StringUtil.format("algorithm:{},hash:{}", algorithm.getName(), AlgorithmUtil.signature(content.getBytes(StandardCharsets.UTF_8), algorithm)));
+        }
+    }
+
+    public static void des(String key, String content) {
+        log.info("########## des 对称加密算法 ###########");
+        log.info(StringUtil.format("des 加密数据:{}", content));
+        // 加密
+        byte[] desEncrypt = AlgorithmUtil.desEncrypt(content.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8));
+        // 转成 string
+        String db = AlgorithmUtil.base64Encode(desEncrypt);
+        log.info(StringUtil.format("des 加密后 byte2string:{}", db));
+        // 解密
+        byte[] desDecrypt = AlgorithmUtil.desDecrypt(AlgorithmUtil.base64Decode(db), key.getBytes(StandardCharsets.UTF_8));
+        log.info(StringUtil.format("des 解密数据:{}", new String(desDecrypt, StandardCharsets.UTF_8)));
+    }
+
+    public static void aes(String key, String content) {
+        log.info("########## aes 对称加密算法 ###########");
+        log.info(StringUtil.format("aes 加密数据:{}", content));
+        // 加密
+        byte[] aesEncrypt = AlgorithmUtil.aesEncrypt(content.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8));
+        // 转成 string
+        String db = AlgorithmUtil.byteToHexStr(aesEncrypt);
+        log.info(StringUtil.format("aes 加密后 byte2string:{}", db));
+        // 解密
+        byte[] aesDecrypt = AlgorithmUtil.aesDecrypt(AlgorithmUtil.byteFromHexStr(db), key.getBytes(StandardCharsets.UTF_8));
+        log.info(StringUtil.format("aes 解密数据:{}", new String(aesDecrypt, StandardCharsets.UTF_8)));
+    }
+
+    public static void rsa(byte[] pub, byte[] pri, String content) {
+        // rsa
+        log.info("########## rsa 非对称加密算法 ###########");
+        log.info(StringUtil.format("rsa 加密数据:{}", content));
+        String pubKey = AlgorithmUtil.base64Encode(pub);
+        String priKey = AlgorithmUtil.base64Encode(pri);
+        log.info(StringUtil.format("rsa public key:{}", pubKey));
+        log.info(StringUtil.format("rsa private key:{}", priKey));
+        // 加密
+        byte[] rsaEncrypt = AlgorithmUtil.rsaEncrypt(content.getBytes(StandardCharsets.UTF_8), AlgorithmUtil.base64Decode(pubKey));
+        // 转成 string
+        String db = AlgorithmUtil.base64Encode(rsaEncrypt);
+        log.info(StringUtil.format("rsa 加密后 byte2string:{}", db));
+        // 解密
+        byte[] rsaDecrypt = AlgorithmUtil.rsaDecrypt(AlgorithmUtil.base64Decode(db), AlgorithmUtil.base64Decode(priKey));
+        log.info(StringUtil.format("rsa 解密数据:{}", new String(rsaDecrypt, StandardCharsets.UTF_8)));
+    }
+
+    public static void urlEncode(String content) {
+        log.info("");
+        log.info("########## url encode ###########");
+        log.info(StringUtil.format("url 加密数据:{}", content));
+        String afterEncode = AlgorithmUtil.uRLEncoderUtf8(content);
+        log.info(StringUtil.format("url 加密结果:{}", afterEncode));
+        log.info(StringUtil.format("url 解密数据:{}", AlgorithmUtil.uRLDecoderUtf8(afterEncode)));
+    }
+
 
     public static void main(String[] args) {
         String content = "22dafsd中国232";
         String key = "afsefsrgrwg11111";
+        String url = "http://aa.com?name=sdff{王小帅}\"&age=20";
 
-        // 散列算法
-        System.out.println("##########hash###########");
-        for (AlgorithmUtil.HashAlgorithm algorithm : AlgorithmUtil.HashAlgorithm.values()) {
-            System.out.println(StringUtil.format("algorithm:{},hash:{}", algorithm.getName(), signature(content.getBytes(StandardCharsets.UTF_8), algorithm)));
-        }
+        hash(content);
 
-        // 对称加密算法
+        des(key, content);
 
-        // des
-        System.out.println("##########des###########");
-        System.out.println(StringUtil.format("des 加密数据:{}", content));
-        byte[] desEncrypt = AlgorithmUtil.desEncrypt(content.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8));
-        // 入库
-        String db = AlgorithmUtil.base64Encode(desEncrypt);
-        System.out.println(StringUtil.format("des 进行入库:{}", db));
+        aes(key, content);
 
-        byte[] desDecrypt = AlgorithmUtil.desDecrypt(AlgorithmUtil.base64Decode(db), key.getBytes(StandardCharsets.UTF_8));
-        System.out.println(StringUtil.format("des 解密数据:{}", new String(desDecrypt, StandardCharsets.UTF_8)));
-
-        // aes
-        System.out.println("##########aes###########");
-        System.out.println(StringUtil.format("aes 加密数据:{}", content));
-        byte[] aesEncrypt = AlgorithmUtil.aesEncrypt(content.getBytes(StandardCharsets.UTF_8), key.getBytes(StandardCharsets.UTF_8));
-        // 入库
-        db = AlgorithmUtil.byteToHexStr(aesEncrypt);
-        System.out.println(StringUtil.format("aes 进行入库:{}", db));
-
-        byte[] aesDecrypt = AlgorithmUtil.aesDecrypt(AlgorithmUtil.byteFromHexStr(db), key.getBytes(StandardCharsets.UTF_8));
-        System.out.println(StringUtil.format("aes 解密数据:{}", new String(aesDecrypt, StandardCharsets.UTF_8)));
-
-        System.out.println("##########url###########");
-        String url = "name=sdff{王小帅}\"";
-        System.out.println(StringUtil.format("url 加密数据:{}", url));
-        System.out.println(StringUtil.format("url 加密结果:{}", AlgorithmUtil.uRLEncoderUtf8(url)));
-        System.out.println(StringUtil.format("url 解密数据:{}", AlgorithmUtil.uRLDecoderUtf8(url)));
-
-        // rsa
-        System.out.println("##########rsa###########");
         List<byte[]> keyPair = AlgorithmUtil.rsaKeyGenerate();
-        String pubKey = AlgorithmUtil.base64Encode(keyPair.get(0));
-        String priKey = AlgorithmUtil.base64Encode(keyPair.get(1));
-        System.out.println(StringUtil.format("rsa public key:{}", pubKey));
-        System.out.println(StringUtil.format(""));
-        System.out.println(StringUtil.format("rsa private key:{}", priKey));
+        rsa(keyPair.get(0), keyPair.get(1), content);
 
-        System.out.println("##########des###########");
-        System.out.println(StringUtil.format("rsa 加密数据:{}", content));
-        byte[] rsaEncrypt = AlgorithmUtil.rsaEncrypt(content.getBytes(StandardCharsets.UTF_8), AlgorithmUtil.base64Decode(pubKey));
-        // 入库
-        db = AlgorithmUtil.base64Encode(rsaEncrypt);
-        System.out.println(StringUtil.format("rsa 进行入库:{}", db));
-
-        byte[] rsaDecrypt = AlgorithmUtil.rsaDecrypt(AlgorithmUtil.base64Decode(db), AlgorithmUtil.base64Decode(priKey));
-        System.out.println(StringUtil.format("rsa 解密数据:{}", new String(rsaDecrypt, StandardCharsets.UTF_8)));
+        log.info("");
+        urlEncode(url);
 
     }
-
 
 }
