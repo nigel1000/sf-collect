39d8623f4933a8a08e5023c16bbe9d1556c8cbf2
中华小当家 <xiaodangjia@gov.cn>
2019-11-01 18:45:54 +0800

    迭代模式&命令模式&备忘录模式&责任链模式

---
+++ collect-common/collect-debug/src/main/java/com/common/collect/debug/design/mode/behave/Iterator.java
@@ -0,0 +1,133 @@
+package com.common.collect.debug.design.mode.behave;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by nijianfeng on 2019/11/1.
+ * <p>
+ * 顺序的访问一个集合而不暴露集合的内部
+ * 内部(内禀)迭代-写在集合类内部的迭代器，因为在类内部可直接获取元素
+ * 外部(外禀)迭代-写在集合类外部的迭代器，需要集合提供获取元素的功能
+ * 静态迭代(拷贝一份集合，迭代期间对原集合不影响)
+ * 动态迭代(迭代器可修改原集合并且原集合修改会导致迭代快速失败)
+ */
+
+@Slf4j
+public class Iterator {
+
+    public static void main(String[] args) {
+        List<String> list = new List<>(new String[]{"1", "2", "3"});
+        for (Itr itr = list.itrForward(); itr.hasNext(); ) {
+            log.info("{}:{}", itr.getClass().getSimpleName(), itr.next());
+        }
+
+        for (Itr itr = list.itrBack(); itr.hasNext(); ) {
+            log.info("{}:{}", itr.getClass().getSimpleName(), itr.next());
+        }
+    }
+
+}
+
+
+interface Collect<T> {
+    Itr<T> itrForward();
+
+    Itr<T> itrBack();
+}
+
+interface ListCollect<T> extends Collect<T> {
+
+    int size();
+
+    T index(int index);
+
+}
+
+class List<T> implements ListCollect<T> {
+
+    private T[] elements;
+
+    public List(T[] elements) {
+        this.elements = elements;
+    }
+
+    @Override
+    public Itr<T> itrForward() {
+        return new ForwardItr<>(this);
+    }
+
+    @Override
+    public Itr<T> itrBack() {
+        return new BackItr<T>(this);
+    }
+
+    @Override
+    public int size() {
+        return elements.length;
+    }
+
+    @Override
+    public T index(int index) {
+        return elements[index];
+    }
+}
+
+interface Itr<T> {
+
+    boolean hasNext();
+
+    T next();
+
+}
+
+// 外部迭代 往前遍历
+class ForwardItr<T> implements Itr<T> {
+
+    private int index;
+    private ListCollect<T> collect;
+
+    public ForwardItr(ListCollect<T> collect) {
+        this.collect = collect;
+        index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean hasNext = (collect.size() != index);
+        if (!hasNext) {
+            index = 0;
+        }
+        return hasNext;
+    }
+
+    @Override
+    public T next() {
+        return collect.index(index++);
+    }
+}
+
+// 外部迭代 往后遍历
+class BackItr<T> implements Itr<T> {
+
+    private int index;
+    private ListCollect<T> collect;
+
+    public BackItr(ListCollect<T> collect) {
+        this.collect = collect;
+        index = collect.size();
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean hasNext = (index > 0);
+        if (!hasNext) {
+            index = collect.size();
+        }
+        return hasNext;
+    }
+
+    @Override
+    public T next() {
+        return collect.index(--index);
+    }
+}
---
+++ collect-common/collect-debug/src/main/java/com/common/collect/debug/design/mode/behave/ResponsibilityChain.java
@@ -0,0 +1,150 @@
+package com.common.collect.debug.design.mode.behave;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by nijianfeng on 2019/11/1.
+ * <p>
+ * 每一个对象对其下家对引用连接起来形成一条链，请求在此链上传递
+ * 降低发出请求的对象和处理请求对象之间的耦合，发出的对象无须知道处理此请求的对象
+ * 责任链模式：特定的处理对象对请求或命令的处理变得不确定
+ * 命令模式：特定的处理对象对一个命令对执行变得确定
+ */
+
+@Slf4j
+public class ResponsibilityChain {
+
+    public static void main(String[] args) {
+        Handler level1Leader = new Level1Leader(null);
+        Handler handler =
+                new Level2Leader(
+                        new Level3Leader(
+                                new Level4Leader(
+                                        level1Leader)));
+        level1Leader.setNext(handler);
+
+        log.info("#########################");
+        handler.handle(new Request(4));
+
+        log.info("#########################");
+        handler.handle(new Request(3));
+
+    }
+}
+
+class Request {
+
+    @Setter
+    @Getter
+    private int state;
+
+    Request(int state) {
+        if (state > 4 || state <= 0) {
+            throw new RuntimeException("状态必须在 1-4");
+        }
+        this.state = state;
+    }
+}
+
+
+abstract class Handler {
+
+    Handler next;
+
+    Handler(Handler next) {
+        this.next = next;
+    }
+
+    void setNext(Handler next) {
+        this.next = next;
+    }
+
+    abstract void handle(Request request);
+
+}
+
+@Slf4j
+class Level1Leader extends Handler {
+
+    Level1Leader(Handler next) {
+        super(next);
+    }
+
+    @Override
+    public void handle(Request request) {
+        if (request.getState() == 0) {
+            return;
+        }
+        if (request.getState() == 1) {
+            request.setState(request.getState() - 1);
+            log.info("通过 Level1Leader 审核");
+        }
+        this.next.handle(request);
+    }
+}
+
+@Slf4j
+class Level2Leader extends Handler {
+
+    Level2Leader(Handler next) {
+        super(next);
+    }
+
+    @Override
+    public void handle(Request request) {
+        if (request.getState() == 0) {
+            return;
+        }
+        if (request.getState() == 2) {
+            request.setState(request.getState() - 1);
+            log.info("通过 Level2Leader 审核");
+        }
+        this.next.handle(request);
+    }
+}
+
+@Slf4j
+class Level3Leader extends Handler {
+
+    Level3Leader(Handler next) {
+        super(next);
+    }
+
+    @Override
+    public void handle(Request request) {
+        if (request.getState() == 0) {
+            return;
+        }
+        if (request.getState() == 3) {
+            request.setState(request.getState() - 1);
+            log.info("通过 Level3Leader 审核");
+        }
+        this.next.handle(request);
+    }
+}
+
+@Slf4j
+class Level4Leader extends Handler {
+
+    Level4Leader(Handler next) {
+        super(next);
+    }
+
+    @Override
+    public void handle(Request request) {
+        if (request.getState() == 0) {
+            return;
+        }
+        if (request.getState() == 4) {
+            request.setState(request.getState() - 1);
+            log.info("通过 Level4Leader 审核");
+        }
+        this.next.handle(request);
+    }
+}
+
+
+
+
